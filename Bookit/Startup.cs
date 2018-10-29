c5df899b410ccc6fea7419daf33b8a716b21435a
Hakim Ghods <ghods.hakim@hotmail.com>
2018-10-29 16:53:20 +0330

    Clean up

--- Bookit/Startup.cs
+++ Bookit/Startup.cs
@@ -19,12 +19,11 @@ namespace Bookit
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
@@ -41,7 +40,6 @@ namespace Bookit
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
